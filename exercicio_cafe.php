35371148f7a2d5290f60b052e2bd7eecf2d83f77
AndreVictordmr <andre.victordmr@gmail.com>
2025-09-22 15:56:49 -0300

    Exercicio cafe

--- exercicio_cafe.php
+++ exercicio_cafe.php
@@ -7,68 +7,14 @@
         <title>Cadastro ninja</title>
     </head>
     <body>
-        <?php 
-            function cadastrarNinja($nome,$cla,$rank,$filhos){
-                
-                $ninja = [
-                    'nome'=>$nome,
-                    'cla'=>$cla,
-                    'nivel'=>$rank,
-                    'missoes'=>[],
-                    'tem_filhos'=>$filhos
-                ];
-                if($rank == 2){
-                    $ninja['missoes'] = ["Rank D"];
-                }elseif($rank == 3){
-                    $ninja['missoes'] = ["Rank D","Rank C"];
-                }elseif($rank == 4){
-                    $ninja['missoes'] = ["Rank D","Rank C",'Rank B'];
-                }elseif($rank == 5){
-                    $ninja['missoes'] = ["Rank D","Rank C","Rank B","Rank A"];
-                }
-                return $ninja; 
-            }
-            $ninja = [
-                 cadastrarNinja('Andre','Uchiha',2,false),
-                 cadastrarNinja('Ten','Hyuga',4,false),
-                 cadastrarNinja('Leo','Nara',5,true)
-            ];
-            function missao($valor){
-                $mensagem ='<p>Sua missão atual e de  ';
-                if($valor == 1){
-                    $mensagem .= "Rank D</p>";
-                }elseif($valor == 2){
-                    $mensagem .= "Rank C</p>";
-                }elseif($valor == 3){
-                    $mensagem .= "Rank B</p>";
-                }elseif($valor == 4){
-                    $mensagem .= "Rank A</p>";
-                }elseif($valor == 5){
-                    $mensagem .= "Rank S</p>";
-                }
-                return $mensagem;
-            }
 
-            function exibirCard($ninja){
-                echo "<h2>Seus Dados</h2>";
-                echo "<p>Nome: ".$ninja['nome']."</p>";
-                echo "<p>Clã: ".$ninja['cla']."</p>";
-                echo "<p>Missoes Realizadas: ".implode(',',$ninja['missoes'])."</p>";
-                echo "<p>Rank: ".$ninja['nivel']."</p>";
-                
-                if($ninja['tem_filhos']){
-                    echo "<p>Possui Filhos: sim </p>";
-                }else{
-                    echo "<p>Possui Filhos: nao </p>";
-                }
-                echo missao($ninja['nivel']);
+       
+        <?php
+        include 'bloco_cafe.php';
+            for($i = 0; $i<count($ninja);$i++){    
+                exibirCard($ninja[$i]);
             }
         ?>
-
-        <main id='card'>
-            <?php
-                exibirCard($ninja[0]);
-            ?>
-        </main>
+       
     </body>
 </html>
