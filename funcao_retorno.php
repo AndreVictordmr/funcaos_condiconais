9776f7f05e7a5b7e31a5445c0aa8ad4234a06b7e
AndreVictordmr <andre.victordmr@gmail.com>
2025-09-23 13:43:43 -0300

    arrumado as pastas

--- funcao_retorno.php
+++ funcao_retorno.php
@@ -3,14 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Funçoes Return</title>
+        <title>Sistema Ninja</title>
     </head>
     <body>
         <?php
             require_once "cabecalho.php";
             
-            require_once "sistema_folha.php";
-            cadatras_Ninja("Andre",4,2);
+            require_once "sistema_cadastro/sistema_folha.php";
+            cadatras_Ninja("Andre",4);
 
 
             require_once "rodape.php";
--- sistema_cadastro/sistema_folha.php
+++ sistema_cadastro/sistema_folha.php
@@ -1,8 +1,7 @@
 <?php
     require_once "bd_cla.php";
-    require_once "sistema_de_missao.php";
    
-    function cadatras_ninja($nome, $valor,$nivel){
+    function cadatras_ninja($nome, $valor){
         echo "<p>Nome: $nome</p>";
         echo claDescricao($valor);
     }
